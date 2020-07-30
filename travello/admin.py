b7e11497fc00ef7b73fcbd07dcd58df093d6cdc6
Alaminpramanik <49738215+Alaminpramanik@users.noreply.github.com>
2020-07-30 16:08:19 +0600

    Delete admin.py

--- travello/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Destination
-from travello.models import Blog
-# Register your models here.
-
-admin.site.register(Destination)
-admin.site.register(Blog)
