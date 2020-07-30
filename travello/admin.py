ec12d1e42fe4243ac04b9fc388e4236932d98c3f
Alaminpramanik <49738215+Alaminpramanik@users.noreply.github.com>
2020-07-30 15:53:52 +0600

    Add files via upload

---
+++ travello/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Destination
+from travello.models import Blog
+# Register your models here.
+
+admin.site.register(Destination)
+admin.site.register(Blog)
